e948afca39ce30fa8567038a5aead2edd95ff870
Daniel Procházka <xproch0d@stud.fit.vutbr.cz>
2020-12-07 13:21:57 +0100

    Examples

--- Examples/var.go
+++ Examples/var.go
@@ -1,7 +1,20 @@
 package main
 
+func test() (int,int){
+    
+}
 
 func main(){
-    a:= "str"
+    a:= 50 / (20 * 30 + 40) + 10
+    b := 10.10
+    _ = 10 + a
+    
+    
+    a,b = test()
+
+    a,b = 10,20
+    b,_ = a, 20+30
+    a = test()
+
 }
 
